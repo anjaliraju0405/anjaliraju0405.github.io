414926a3e5283145877c154844b456b3de684dd5
anjaliraju0405 <anjaliraju0405@gmail.com>
2022-04-21 19:11:33 -0400

    commit to main

---
+++ lab-2022/assignment-10/bubblewrap.js
@@ -0,0 +1,43 @@
+let container = document.querySelector(".grid-container");
+
+let clearDrawing = document.querySelector("#clearDrawing");
+
+
+let isPainting = false;
+
+for (let i = 0; i < 16 * 16; i++){
+  
+ 
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("pixel");
+  
+ 
+  newDiv.addEventListener("mousedown", function(){
+    isPainting = true;
+    newDiv.style.backgroundColor = "skyblue";
+  });
+  
+  
+
+  newDiv.addEventListener("mouseenter", function(){
+
+    if (isPainting){
+      newDiv.style.backgroundColor = "skyblue";
+    }
+    
+  });
+  
+
+  newDiv.addEventListener("mouseup", function(){
+  
+    isPainting = false;
+  });
+  
+
+  clearDrawing.addEventListener("click", function(){
+    newDiv.style.background = "white";
+  })
+  
+  
+  container.appendChild(newDiv);
+}
